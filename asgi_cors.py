4ee1fbe00f88a606daea7c959d0bf5350379d516
Thorben Westerhuys <1425577+n0rdlicht@users.noreply.github.com>
2024-04-08 05:10:52 +0200

    Add option to modify access-control-allow-headers and -methods (#7)
    * add access-control-allow-headers and -methods as configurable options

--- asgi_cors.py
+++ asgi_cors.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 
 def asgi_cors_decorator(
-    allow_all=False, hosts=None, host_wildcards=None, callback=None
+    allow_all=False, hosts=None, host_wildcards=None, callback=None, headers=None, methods=None
 ):
     hosts = hosts or []
     host_wildcards = host_wildcards or []
@@ -12,6 +12,12 @@ def asgi_cors_decorator(
     hosts = set(h.encode("utf8") if isinstance(h, str) else h for h in hosts)
     host_wildcards = [
         h.encode("utf8") if isinstance(h, str) else h for h in host_wildcards
+    ]
+    headers = [
+        h.encode("utf8") if isinstance(h, str) else h for h in headers
+    ]
+    methods = [
+        h.encode("utf8") if isinstance(h, str) else h for h in methods
     ]
 
     if any(h.endswith(b"/") for h in (hosts or [])) or any(
@@ -43,6 +49,13 @@ def asgi_cors_decorator(
                                 matches_callback = callback(incoming_origin)
                             if matches_hosts or matches_wildcards or matches_callback:
                                 access_control_allow_origin = incoming_origin
+                    access_control_allow_headers = ['content-type']
+                    if headers:
+                        access_control_allow_headers = headers
+                    access_control_allow_methods = ['GET']
+                    if methods:
+                        access_control_allow_methods = methods
+
                     if access_control_allow_origin is not None:
                         # Construct a new event with new headers
                         event = {
@@ -51,12 +64,24 @@ def asgi_cors_decorator(
                             "headers": [
                                 p
                                 for p in original_headers
-                                if p[0] != b"access-control-allow-origin"
+                                if p[0] != b"access-control-allow-origin" and p[0] != b"access-control-allow-headers" and p[0] != b"access-control-allow-methods"
                             ]
                             + [
                                 [
                                     b"access-control-allow-origin",
                                     access_control_allow_origin,
+                                ]
+                            ]
+                            + [
+                                [
+                                    b"access-control-allow-headers",
+                                    b", ".join(access_control_allow_headers),
+                                ]
+                            ]
+                            + [
+                                [
+                                    b"access-control-allow-methods",
+                                    b", ".join(access_control_allow_methods),
                                 ]
                             ],
                         }
@@ -69,5 +94,5 @@ def asgi_cors_decorator(
     return _asgi_cors_decorator
 
 
-def asgi_cors(app, allow_all=False, hosts=None, host_wildcards=None, callback=None):
-    return asgi_cors_decorator(allow_all, hosts, host_wildcards, callback)(app)
+def asgi_cors(app, allow_all=False, hosts=None, host_wildcards=None, callback=None, headers=None, methods=None):
+    return asgi_cors_decorator(allow_all, hosts, host_wildcards, callback, headers, methods)(app)
